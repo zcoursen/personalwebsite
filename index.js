ac046d8860cf45fd829be04df1c9bc4523a06799
zcoursen <nuzack@gmail.com>
2019-04-15 18:44:29 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { BrowserRouter as Router} from "react-router-dom";
 import './css/index.css';
 import * as serviceWorker from './JS/serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+<Router>
+    <App />
+</Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
